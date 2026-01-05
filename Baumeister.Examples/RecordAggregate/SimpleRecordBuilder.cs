7bc0133164ba0acb4d47dc18ea07198cd75bcade
Andre Orlowski <andre.orlowski@gmx.de>
2026-01-05 21:11:45 +0100

    Added unit test to verify support of record types

---
+++ Baumeister.Examples/RecordAggregate/SimpleRecordBuilder.cs
@@ -0,0 +1,8 @@
+using Baumeister.Abstractions.Building;
+
+namespace Baumeister.Examples.RecordAggregate
+{
+    public partial class SimpleRecordBuilder : BuilderBase<SimpleRecord>
+    {
+    }
+}
